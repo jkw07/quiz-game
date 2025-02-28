4700de3712570604f9022910e9a839fd2557780d
Justyna W <jkwodecka@gmail.com>
2025-02-28 18:15:00 +0100

    change styles

--- src/components/HalfPieChart.tsx
+++ src/components/HalfPieChart.tsx
@@ -7,6 +7,7 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import { COLORS } from "../constans/pieChartColors";
+import "../styles/HalfPieChart.scss";
 
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
@@ -18,31 +19,36 @@ export const HalfPieChart = () => {
   );
 
   return (
-    <ResponsiveContainer width="100%" height={150}>
-      <PieChart>
-        <Pie
-          data={data}
-          cx="50%"
-          cy="100%"
-          startAngle={180}
-          endAngle={0}
-          innerRadius={50}
-          outerRadius={80}
-          fill="#8884d8"
-          paddingAngle={2}
-          dataKey="value"
-        >
-          {data.map((_entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-        <Tooltip />
-        <Legend
-          wrapperStyle={{ width: "100%" }}
-          verticalAlign="top"
-          align="center"
-        />
-      </PieChart>
-    </ResponsiveContainer>
+    <>
+      <ResponsiveContainer width="100%" height={150}>
+        <PieChart>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="100%"
+            startAngle={180}
+            endAngle={0}
+            innerRadius={50}
+            outerRadius={80}
+            fill="#8884d8"
+            paddingAngle={2}
+            dataKey="value"
+          >
+            {data.map((_entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend
+            wrapperStyle={{ width: "100%" }}
+            verticalAlign="top"
+            align="center"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </>
   );
 };
