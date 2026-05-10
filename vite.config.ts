91c6211e872f6d219eeb0bf798fe328317c61a1c
Justyna W <jkwodecka@gmail.com>
2026-05-10 19:37:30 +0200

    add gh-pages deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig as defineViteConfig } from 'vite';
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vitest/config'
+import { defineConfig as defineViteConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineViteConfig({
+  base: "/quiz-game/",
   plugins: [react()],
   server: {
     allowedHosts: [
-      '5173-jkw07-quizgame-adccddtjlp3.ws-eu117.gitpod.io', // Add your host here
-      'localhost', // Optionally add localhost if it's not already included
+      "5173-jkw07-quizgame-adccddtjlp3.ws-eu117.gitpod.io", // Add your host here
+      "localhost", // Optionally add localhost if it's not already included
     ],
   },
   test: {
     globals: true,
-    environment: 'jsdom', // Simulates DOM in React tests
-    setupFiles: './src/setupTests.ts', // You can add setup for tests (e.g., RTL)
+    environment: "jsdom", // Simulates DOM in React tests
+    setupFiles: "./src/setupTests.ts", // You can add setup for tests (e.g., RTL)
   },
-  },
-);
+});
